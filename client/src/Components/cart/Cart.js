f23fff234a38c619c47bc8ceff5f7162dd90a545
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-02-17 09:23:17 +0530

    frontend deployment

--- client/src/Components/cart/Cart.js
+++ client/src/Components/cart/Cart.js
@@ -35,12 +35,12 @@ export default function Cart() {
     const addtocart = async (id) => {
         const checkRes = await fetch(`https://amazon-clone-sepia-rho.vercel.app/addcart/${id}`, {
             method: "POST",
+            credentials: "include",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json"
             },
             body:JSON.stringify(prod),
-            credentials: "include"
         })
         console.log(checkRes)
         // const data1 = await checkRes.json();
