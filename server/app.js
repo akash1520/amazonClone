66da06dc8ba8d72607636ac31cfa4b3e8c95a04d
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-02-17 16:22:13 +0530

    backend deployment bug fixes

--- server/app.js
+++ server/app.js
@@ -7,13 +7,8 @@ const DefaultData= require("./DefaultData")
 const router = require("./routes/router.js")
 const cookieParser = require("cookie-parser")
 
-const corsOptions = {
-    origin: true, //included origin as true
-    credentials: true, //included credentials as true
-};
-
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors({ origin: true, credentials: true }))
 app.use(cookieParser(""))
 app.use(express.static('public'))
 app.use(router)
