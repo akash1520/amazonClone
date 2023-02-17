6736153819a111a9a3c4c9cd969ac547e7cec05a
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-02-17 19:09:10 +0530

    backend deployment bug fixes

--- server/routes/router.js
+++ server/routes/router.js
@@ -92,9 +92,9 @@ router.post("/login", async (req, res) => {
                  // console.log(token);
                 
                  //cookie generation
-                res.cookie('amazonWeb',token)
-                // res.cookie('amazonWeb',token, { path:'/',expires: new Date(new Date.now() + 3600000) });
-                res.status(200).json(userLogin).send("cookie has been sent!!")
+                // res.cookie('amazonWeb',token)
+                res.cookie('amazonWeb',token, { path:'/',expires: new Date(new Date.now() + 3600000) });
+                res.status(200).json(userLogin)
             }
 
         } else {
