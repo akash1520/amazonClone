737edfaf71b682aa8f86a1e5192df087c6a856d4
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2023-04-03 11:18:17 +0530

    Separating front end and backend

--- server/db/conn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const DB=process.env.DATABASE
-
-mongoose.set('strictQuery', false);
-mongoose.connect(DB).then(()=>console.log("data base connected")).catch((error)=>console.log("error"+error.message))
